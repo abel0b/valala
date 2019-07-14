cf9ad6e5ef197255877d64ac40ebf4cb1b97c055
Abel Calluaud <abelcd@protonmail.com>
2019-07-15 01:51:06 +0200

    fix release script

--- ci/release.sh
+++ ci/release.sh
@@ -10,11 +10,11 @@ function release {
     workdir=`mktemp -d`
     cargo build --release --target "$1"
     cp -r "$workspace/client/res" "$workspace/client/settings.ron" "$workdir"
-    cp "$workspace/target/$1/release/valala-client" "$workdir/$2"
-    tar czf "$workspace/build/valala-$version-$3.tar.gz" -C "$workdir" res settings.ron $2
+    cp "$workspace/target/$1/release/$2" "$workdir"
+    tar czf "$workspace/build/valala-$version-$3.tar.gz" -C "$workdir" .
 }
 
-release "x86_64-unknown-linux-gnu" "valala" "linux"
-release "x86_64-pc-windows-gnu" "Valala.exe" "windows"
+release "x86_64-unknown-linux-gnu" "valala-client" "linux"
+release "x86_64-pc-windows-gnu" "valala-client.exe" "windows"
 
 cp "$workspace/target/x86_64-unknown-linux-gnu/release/valala-server" "$workspace/build/valala-server-$version"
